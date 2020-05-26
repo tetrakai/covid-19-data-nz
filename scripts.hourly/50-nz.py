a1be63a33548a1f2862b5e2886ca83659dcbcd44
Karla <karla.no.spam@gmail.com>
2020-05-26 14:38:51 +1000

    Update NZ parsing

--- scripts.hourly/50-nz.py
+++ scripts.hourly/50-nz.py
@@ -107,6 +107,7 @@ def get_timeseries_data(base_url):
         r'.*as having recovered from COVID-19, an increase of \w+ on yesterday, for a total of (?P<recovered>[\d,]+)\..*',
         r'.*no change to the number of (?:people )?recovered (?:cases which remain )?at (?P<recovered>[\d,]+)[\. ].*',
         r'.*taking recoveries to (?P<recovered>[\d,]+)\..*',
+        r'.*we can report \w+ new recovered cases taking the total to (?P<recovered>[\d,]+)\..*',
       ],
       'confirmed': [
         r'.*This means the current national total is (?P<confirmed>[\d,]+)[,\.].*',
